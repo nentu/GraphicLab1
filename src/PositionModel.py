04342bc9ae99b9fc04c47b24c2d0c69bc2119633
Zaytsev Artem <zajtsev.artjom@samlit.net>
2025-09-26 12:52:21 +0300

    Fix move amd rotate

--- src/PositionModel.py
+++ src/PositionModel.py
@@ -1,26 +1,30 @@
 from imports import Model, draw_model, rotate
+import numpy as np
 
 
 class PositionModel:
-    def __init__(self, model, xyz, ypr, intrinsic_matrix):
+    def __init__(self, model, intrinsic_matrix):
         self.model = model
-        self.xyz = xyz
-        self.ypr = ypr
         self.intrinsic_matrix = intrinsic_matrix
+        self.global_coord = np.array([0, 0, 0])
 
-        self.move(*xyz)
-        self.rotate(*ypr)
-
-    def move(self, x=0, y=0, z=0):
+    def _move(self, x=0, y=0, z=0, move_global=True):
         for i, shift in {
             0: x,
-            1: y,
-            2: z,
+            2: y,
+            1: -z,
         }.items():
             self.model.vertex_list[:, i] += shift
+        if move_global:
+            self.global_coord += [x, y, z]
+
+    def move(self, x=0, y=0, z=0):
+        self._move(x, y, z, True)
 
     def rotate(self, y=0, p=0, r=0):
-        rotate(self.model, y, p, r)
+        self._move(*(-self.global_coord), move_global=False)
+        rotate(self.model, p, y, r)
+        self._move(*self.global_coord, move_global=False)
 
     def draw_model(self, plane):
         projected_model = self.model.apply_transform(self.intrinsic_matrix)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from imports import (
-    draw_model,
     get_depth_map,
     ObjModel,
     get_intrinsic_matrix,
-    rotate,
     Cube,
     Camera,
 )
 import numpy as np
 import cv2
+from PositionModel import PositionModel
 
 if __name__ == "__main__":
     plane_shape = np.array([640, 640])
@@ -17,34 +16,22 @@ if __name__ == "__main__":
     intrinsic_matrix = get_intrinsic_matrix(*camera_angle, *plane_shape)
     # print(angle_coordinates)
 
-    shift = 0
-    rotation = 180
+    model = PositionModel(
+        model=Camera(100),
+        intrinsic_matrix=intrinsic_matrix,
+    )
 
-    h = 0
-    k = 1
+    model.move(y=60)
 
     while True:
         plane = np.zeros(shape=(*plane_shape, 3))
 
-        model = Camera(99)
+        model.rotate(r=0.3)
 
-        # model.vertex_list[:, 1] -= 0.5
-
-        rotate(model, 180, rotation, 0)
-        model.vertex_list[:, 2] += 300 + shift
-
-        # plane += 255
-
-        projected_model = model.apply_transform(intrinsic_matrix)
-
-        draw_model(plane, projected_model)
+        model.draw_model(plane)
 
         cv2.imshow("depth", plane)
         if cv2.waitKey(1) == ord("q"):
             break
-
-        shift += 1
-        rotation += 0.2
-        # k += 2e-3
 
     cv2.destroyAllWindows()
